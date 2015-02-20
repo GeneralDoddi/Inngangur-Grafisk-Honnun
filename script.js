d69a6d2c20a2ce0b37c18d07c99053dc5b1be7f6
GeneralDoddi <doddisti@gmail.com>
2015-02-20 20:58:02 +0000

    done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 /**
  * Created by thordurth on 18.2.2015.
  */
+$( document ).ready(function(){
+
+    $(".notes div").hover(function(){
+        $("#"+$(this).attr('id')+"Text").fadeIn();
+    }, function(){
+        $("#"+$(this).attr('id')+"Text").fadeOut();
+    });
+
+});
